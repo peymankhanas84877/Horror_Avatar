f8f809af6060afeab462fd0ba3a39fbe3dbce5cb
peymankhanas8487 <peymankhanas8487@yahoo.com>
2015-11-24 13:30:36 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-    sudo_users = { PeymanKhanas , 46613419 },
+    sudo_users = { PeymanKhanas , 75000456 },
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
